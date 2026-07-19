5574af567479a6aacc58415d541c546cf2f7d038
ghbasel20-rgb <gh.basel20@gmail.com>
2026-07-19 11:12:55 +0300

    fixing fonts

--- app/Buildprofileskills.jsx
+++ app/Buildprofileskills.jsx
@@ -194,7 +194,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#0a445c',
     marginBottom: 30,
-    lineHeight: 42,
+    lineHeight: 52,
   },
   chipsContainer: {
     flexDirection: 'row',
--- app/TopMatches.jsx
+++ app/TopMatches.jsx
@@ -127,7 +127,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#0a445c',
     marginBottom: 30,
-    lineHeight: 42,
+    lineHeight: 52,
   },
   matchesContainer: {
     gap: 16,
